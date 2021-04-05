2144ae2eb46da713b76b0ba65c5b935dde4ccae1
yd4825 <64185636+yd4825@users.noreply.github.com>
2021-04-06 00:32:39 +0900

    공룡_런
    흔한 공룡 런게임

---
+++ Dino_Run/Assets/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public float Level(int score)
+    {
+        int defaultSpeed = -6;
+        int increSpeed = (score * -1) / 100;
+
+        if (increSpeed < -4)
+            increSpeed = -4;
+
+        return defaultSpeed + increSpeed;
+    }
+}
